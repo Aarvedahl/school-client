33f3f6b1f9e7b51798899c41d0dafe789acde7ee
Alex <alex.arvedahl@gmail.com>
2017-11-15 16:12:27 +0100

    first commmit

---
+++ src/main/java/io/github/aarvedahl/StudentBean.java
@@ -0,0 +1,14 @@
+package io.github.aarvedahl;
+
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class StudentBean implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private List<Student>
+}
