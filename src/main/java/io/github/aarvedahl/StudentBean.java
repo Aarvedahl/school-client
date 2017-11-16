8dc207d68e4c6434edf1f0f004863675dfbed086
Alex <alex.arvedahl@gmail.com>
2017-11-16 11:47:18 +0100

    Displaying all students and their courses

--- src/main/java/io/github/aarvedahl/StudentBean.java
+++ src/main/java/io/github/aarvedahl/StudentBean.java
@@ -3,12 +3,26 @@ package io.github.aarvedahl;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @ViewScoped
 public class StudentBean implements Serializable{
     private static final long serialVersionUID = 1L;
 
-    private List<Student>
+    private List<DtoStudent> studentList;
+    private static Client client = ClientBuilder.newClient();
+    private static WebTarget baseTarget = client.target("http://localhost:8080/SchoolManagement/studentService/list");
+
+    public List<DtoStudent> getStudentList() {
+        if(studentList == null) {
+            studentList = baseTarget.request().get(new GenericType<List<DtoStudent>>() {});
+        }
+        return studentList;
+    }
 }
