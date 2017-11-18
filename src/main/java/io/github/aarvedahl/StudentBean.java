eb2c569399c80e66f09ee0beae399d69e26ec35d
Alex <alex.arvedahl@gmail.com>
2017-11-18 19:34:35 +0100

    login working with file realm

--- src/main/java/io/github/aarvedahl/StudentBean.java
+++ src/main/java/io/github/aarvedahl/StudentBean.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Named
 @ViewScoped
-@DeclareRoles({"admin"})
+@DeclareRoles({"admin", "employee"})
 public class StudentBean implements Serializable{
     private static final long serialVersionUID = 1L;
 
